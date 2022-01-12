d929fa0d9ec6b0aa7a9a8ef1366458b9e2867eb6
luiggy macias <ferrinluiggy@gmail.com>
2022-01-12 08:49:45 -0500

    adding modules

--- backend/src/express.ts
+++ backend/src/express.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import indexRoute from "./routes/index.route";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import compress from "compression";
+import helmet from "helmet";
 const app = express();
 
 // middlewares
 app.use(express.json());
+//express json with url encoded
+app.use(express.urlencoded({ extended: true }));
 app.use(cors()); //nunca te olvides de poner esto, si es que vas a usar las api de otro lado, osea de otro dominio o proxy
 
 //routes
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import app from "./app";
+import app from "./express";
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
