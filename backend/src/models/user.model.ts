ddbbd1ebc4d474cde3fe1089829641ecf7600110
luiggy macias <ferrinluiggy@gmail.com>
2022-01-12 11:41:31 -0500

    adding properties to the user schema

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,5 +1,21 @@
-import { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  name: { type: String, trim: true, required: [true, "Name is required"] },
+  email: {
+    type: String,
+    trim: true,
+    unique: true, // the email must be unique
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ], // and the email must be a valid email
+    required: [true, "Email already exists"],
+  },
+  created: { type: Date, default: Date.now }, // the type of the field is Date, and the default value is the current date
+  updated: Date, // the date is updated automatically
+  hashed_password: { type: String, required: [true, "Password is required"] },
+  salt: String,
+});
 
-export default UserSchema;
+export default model("User", UserSchema);
