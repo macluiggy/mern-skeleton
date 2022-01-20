a74b9fb849b6bd4fdaed490e2fc0320093322c21
luiggy macias <ferrinluiggy@gmail.com>
2022-01-20 14:34:40 -0500

    finishing the users users comopnents

--- frontend/src/core/Home.tsx
+++ frontend/src/core/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core";
 import { CardContent, Card, CardMedia, Typography } from "@material-ui/core";
 import unicornbikeImg from "./../assets/images/unicornbike.jpg";
+import { Link } from "react-router-dom";
 // const unicornbikeImg = require("./../assets/images/unicornbike.jpg");
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -34,6 +35,7 @@ export default function Home() {
       />
       <CardContent>
         <Typography>Welcome to the MERN Skeleton home page.</Typography>
+        <Link to="/users">Users</Link>
       </CardContent>
     </Card>
   );
